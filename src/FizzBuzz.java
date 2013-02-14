364c5f5770a31240b99a36c10cf534609f7d4bc2
Shisu <shisu0306@gmail.com>
2013-02-15 00:51:35 +0900

    FizzBuzz on Java

---
+++ src/FizzBuzz.java
@@ -0,0 +1,17 @@
+import java.*;
+
+public class FizzBuzz {
+	public static void main(String []args){
+		int i;
+		
+		for(i=1; i<=100; i++) {
+			if(i % 15 == 0) {
+				System.out.println(+ i + ", FizzBuzz");
+			}else if(i % 3 == 0){
+				System.out.println(+ i + ", Fizz");
+			}else if(i % 5 == 0){
+				System.out.println(+ i + ", Buzz");
+			}
+		}
+	}
+}
